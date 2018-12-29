3fae22cbf21eb06b8ac7237a406ae22a24c754e4
AnkurYadav619 <yadavankur619@gmail.com>
2018-12-29 12:53:33 +0530

    Update yard6.cs

--- WindowsFormsApp1/yard6.cs
+++ WindowsFormsApp1/yard6.cs
@@ -16,5 +16,150 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        string yard = "EXIM,COASTAL";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char col = 'A';
+            int row = 1;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char col = 'A';
+            int row = 2;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            char col = 'A';
+            int row = 3;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            char col = 'A';
+            int row = 4;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            char col = 'B';
+            int row = 1;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            char col = 'B';
+            int row = 2;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            char col = 'B';
+            int row = 3;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            char col = 'B';
+            int row = 4;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            char col = 'C';
+            int row = 1;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            char col = 'C';
+            int row = 2;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            char col = 'C';
+            int row = 3;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            char col = 'C';
+            int row = 4;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            char col = 'D';
+            int row = 1;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            char col = 'D';
+            int row = 2;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            char col = 'D';
+            int row = 3;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            char col = 'D';
+            int row = 4;
+            container c = new container(yard, col, row);
+            this.Hide();
+            c.ShowDialog();
+        }
     }
 }
